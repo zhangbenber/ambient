64b7eeb9ded20eb98922b961dd675d2b415b56c6
benber <zhangbenber@qq.com>
2017-11-10 20:45:25 +0800

    Fix controller movement

--- src/Controller.ts
+++ src/Controller.ts
@@ -2,27 +2,56 @@ import Clip from './Clip'
 import Sound from './Sound'
 import { field } from './app'
 
+const padding = 50
+let downCtrl: Controller = null
+let downPos: Position = null
+
+let getPos = (e: MouseEvent): Position => ({
+	x: e.clientX / (field.clientWidth - padding * 2),
+	y: e.clientY / (field.clientHeight - padding * 2)
+})
+
+field.onmousedown = (e: MouseEvent) => {
+	downPos = getPos(e)
+}
+
+field.onmousemove = (e: MouseEvent) => {
+	if (downPos && downCtrl) {
+		let pos: Position = getPos(e)
+		downCtrl.pos = new Position(
+			Math.max(0, Math.min(1, downCtrl.pos.x - downPos.x + pos.x)),
+			Math.max(0, Math.min(1, downCtrl.pos.y - downPos.y + pos.y))
+		)
+		downPos = getPos(e)
+		downCtrl.update()
+	}
+}
+
+field.onmouseup = (e: MouseEvent) => {
+	if (downCtrl) {
+		downPos = null
+		downCtrl = null
+	}
+}
+
 class Position {
 	constructor(public x: number = 0.5, public y: number = 0.5) { }
 }
 
-export default class {
+class Controller {
 	private elem: HTMLDivElement = document.createElement('div')
 	private sound: Sound
-	private pos: Position = new Position()
-	private downPos: Position = null
-	
-	private readonly padding = 50
-	
+	public pos: Position = new Position()
+
 	constructor(
 		readonly clip: Clip,
 	) {
 		this.elem.className = 'ctrl'
 		this.elem.style.backgroundColor = clip.color
 		this.elem.style.backgroundImage = `url(assets/${clip.icon})`
-		this.elem.onmousedown = this.mousedownHandler
-		this.elem.onmousemove = this.mousemoveHandler
-		this.elem.onmouseup = this.mouseupHandler
+		this.elem.onmousedown = () => {
+			downCtrl = this
+		}
 		field.appendChild(this.elem)
 
 		this.sound = new Sound(clip)
@@ -35,8 +64,8 @@ export default class {
 	}
 
 	update(): void {
-		this.elem.style.left = `${(field.clientWidth - this.padding * 2) * this.pos.x + this.padding}px`
-		this.elem.style.top = `${(field.clientHeight - this.padding * 2) * this.pos.y + this.padding}px`
+		this.elem.style.left = `${(field.clientWidth - padding * 2) * this.pos.x + padding}px`
+		this.elem.style.top = `${(field.clientHeight - padding * 2) * this.pos.y + padding}px`
 		if (this.sound) {
 			this.sound.volume = (1 - this.pos.y) ** 1.5
 			this.sound.pan = this.pos.x * 2 - 1
@@ -53,29 +82,5 @@ export default class {
 		}, 1000)
 	}
 
-	private getPos = (e: MouseEvent): Position => ({
-		x: e.pageX / (field.clientWidth - this.padding * 2),
-		y: e.pageY / (field.clientHeight - this.padding * 2)
-	})
-
-	private mousedownHandler = (e: MouseEvent) => {
-		this.downPos = this.getPos(e)
-		console.log(1)
-	}
-
-	private mousemoveHandler = (e: MouseEvent) => {
-		if (this.downPos) {
-			let pos: Position = this.getPos(e)
-			this.pos = new Position(
-				Math.max(0, Math.min(1, this.pos.x - this.downPos.x + pos.x)),
-				Math.max(0, Math.min(1, this.pos.y - this.downPos.y + pos.y))
-			)
-			this.downPos = this.getPos(e)
-			this.update()
-		}
-	}
-
-	private mouseupHandler = (e: MouseEvent) => {
-		this.downPos = null
-	}
 }
+export default Controller
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import Clip from './Clip'
 import Sound from './Sound'
-import Controller from './Controller'
 
 let context: AudioContext = new AudioContext()
 let field: HTMLDivElement = document.createElement('div')
@@ -11,6 +10,7 @@ let clips: Array<Clip> = [
 	new Clip('White Noise')
 ]
 
+import Controller from './Controller'
 let ctrl = new Controller(clips[0])
 
 export { context, field }
